3788ec76f2d788015379477cee1a1db4f773b8d5
Roman Balandin <tinkerscript@yandex.ru>
2018-10-24 16:21:27 +0500

    styling

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends:['airbnb'],
+    extends: ['airbnb'],
     rules: {
         'eol-last': 0,
         'func-names': 0,
@@ -14,6 +14,6 @@ module.exports = {
         'jsx-a11y/anchor-has-content': 0,
         'jsx-a11y/click-events-have-key-events': 0,
         'jsx-a11y/no-static-element-interactions': 0,
-        'jsx-a11y/anchor-is-valid': 0
-    }
+        'jsx-a11y/anchor-is-valid': 0,
+    },
 };
