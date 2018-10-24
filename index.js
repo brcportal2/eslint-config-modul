67f1fe4699610390a4e1824c1e5e0123217e9df2
Roman Balandin <tinkerscript@yandex.ru>
2018-10-24 15:59:24 +0500

    basic stuff

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends:['airbnb'],
+    rules: {
+        "eol-last": 0,
+        "func-names": 0,
+        "spaced-comment": 0,
+        "import/prefer-default-export": 0,
+        "class-methods-use-this": 0,
+        "indent": ["error", 4],
+        "react/jsx-indent": 0,
+        "react/jsx-indent-props": 0,
+        "react/require-default-props": 0,
+        "react/jsx-one-expression-per-line": 0,
+        "jsx-a11y/anchor-has-content": 0,
+        "jsx-a11y/click-events-have-key-events": 0,
+        "jsx-a11y/no-static-element-interactions": 0,
+        "jsx-a11y/anchor-is-valid": 0
+    }
+};
